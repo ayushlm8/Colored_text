9ae7a4fe0b28a388c1b54a50d7a56000558fea19
Ayush <ayush.yadu8920@gmail.com>
2024-06-23 21:16:39 +0530

    first commit

---
+++ color_text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore,Back,Style
+colorama.init()
+print(Fore.GREEN+"HELLO FRIENDS WELCOME TO OUR Let's learn coding coaching")
+print(Back.BLUE+"Hi Friends my name Ayush")
+print(Fore.RED+Back.GREEN+"This Is Our Let's learn coding coaching")
+print(Style.BRIGHT+"Come and visit us ")
